fa554a6fe3247036a226e444d9be40d60bb858f4
Danny Garcia <dannygarcia.me@gmail.com>
2017-07-08 16:52:43 -0400

    Rename ViewController to TipViewController

--- Gratuity/TipViewController.swift
+++ Gratuity/TipViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TipViewController: UIViewController {
   
   @IBOutlet weak var billAmt: UITextField!
   @IBOutlet weak var gratuityAmt: UILabel!
