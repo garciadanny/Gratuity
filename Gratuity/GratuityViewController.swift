2e0c53752967b01f61cee654c5f0c888aa60c0de
Danny Garcia <dannygarcia.me@gmail.com>
2017-07-09 14:54:21 -0400

    Remember the bill amount across app restarts
    Remember the bill amount across app restarts. After an 10 minutes,
    clear the state.

--- Gratuity/GratuityViewController.swift
+++ Gratuity/GratuityViewController.swift
@@ -16,27 +16,15 @@ class GratuityViewController: UIViewController {
   @IBOutlet weak var gratuityControl: UISegmentedControl!
   
   let defaults = UserDefaults.standard
+  let gratuityPercentages = [0.18, 0.20, 0.25]
+  let sessionDataTimeLimit: Double = 600
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    if let defaultGratuity = defaults.object(forKey: "defaultGratuityIndex") {
-      gratuityControl.selectedSegmentIndex = defaultGratuity as! Int
-    }
+    setPreviousSessionData()
     defaults.addObserver(self, forKeyPath: "defaultGratuityIndex", options: .new, context: nil)
   }
-  
-  override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-    if keyPath == "defaultGratuityIndex" {
-      gratuityControl.selectedSegmentIndex = defaults.integer(forKey: "defaultGratuityIndex")
-      calculateGratuity()
-    }
-  }
-  
-  deinit {
-    defaults.removeObserver(self, forKeyPath: "defaultGratuityIndex")
-  }
-  
+
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
   }
@@ -45,15 +33,39 @@ class GratuityViewController: UIViewController {
     view.endEditing(true)
   }
   
+  override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+    if keyPath == "defaultGratuityIndex" {
+      gratuityControl.selectedSegmentIndex = defaults.integer(forKey: "defaultGratuityIndex")
+      calculateGratuity()
+    }
+  }
+
+  deinit {
+    defaults.removeObserver(self, forKeyPath: "defaultGratuityIndex")
+  }
+ 
   @IBAction func calculateGratuity() {
-    let gratuityPercentages = [0.18, 0.20, 0.25]
-    
     let bill = Double(billAmt.text!) ?? 0
     let gratuity = bill * gratuityPercentages[gratuityControl.selectedSegmentIndex]
     let total = bill + gratuity
     
     gratuityAmt.text = String(format: "$%.2f", gratuity)
     totalAmt.text = String(format: "$%.2f", total)
+    
+    defaults.set(bill, forKey: "storedBillAmt")
+  }
+  
+  func setPreviousSessionData() {
+    if let defaultGratuity = defaults.object(forKey: "defaultGratuityIndex") {
+      gratuityControl.selectedSegmentIndex = defaultGratuity as! Int
+    }
+    
+    if let previousSession = defaults.object(forKey: "previousSessionDate") as? Date {
+      let currentSession = Date()
+      if currentSession.timeIntervalSince(previousSession) < sessionDataTimeLimit {
+        billAmt.text = defaults.string(forKey: "storedBillAmt")
+      }
+    }
   }
 }
 
--- README.md
+++ README.md
@@ -6,12 +6,14 @@ Time spent: 2 hours (on and off)
 
 ## Features
 
-- [ ] Required: Basic functionality calculates gratuity amount based on bill amount
+- [x] Required: Basic functionality calculates gratuity amount based on bill amount
 and gratuity percentage selected.
-- [ ] Required: Settings page that allows a user to set a default gratuity amount.
+- [x] Required: Settings page that allows a user to set a default gratuity amount.
 After a user visits the settings page, the gratuity percentage in the home screen
 only changes if a new default value was selected. Visiting the settings view
 displays the current default gratuity amount.
+- [x] Optional: Remember the bill amount across app restarts.
+After an 10 minutes, clear the state.
 
 ## Demo
 ![App Demo](demo.gif)
